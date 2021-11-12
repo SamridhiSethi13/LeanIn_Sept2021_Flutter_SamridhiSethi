d10f4785da57e66fbf4f70344e9d2696c6ccdd07
Samridhi Sethi <90918405+SamridhiSethi13@users.noreply.github.com>
2021-11-12 22:28:39 +0530

    Update result.dart

--- MILESTONE 2/result.dart
+++ MILESTONE 2/result.dart
@@ -8,31 +8,60 @@ class Result extends StatelessWidget {
 
   // ignore: use_key_in_widget_constructors
   const Result(this._sum);
-  Widget res(int _sum)
-  {
-    if(_sum >= 10)
-    {
-      return const Text("You Passed !!", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green, fontSize: 40.0,), textAlign: TextAlign.center,);
+  Widget res(int _sum) {
+    if (_sum >= 10) {
+      return const Text(
+        "You Passed !!",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.green,
+          fontSize: 40.0,
+        ),
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        "You Failed !!",
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.red, fontSize: 40.0),
+        textAlign: TextAlign.center,
+      );
     }
-    else
-    {
-      return const Text("You Failed !!", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 40.0),textAlign: TextAlign.center,);
-    }      
   }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 200, horizontal: 80),
-      child: Column(
-        children: [
-          Text(
-            'Score : ' + _sum.toString(),
-            style: const TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "GK QUIZ",
+            style: TextStyle(
+              fontSize: 50.0,
+              color: Colors.pink,
+              fontWeight: FontWeight.bold,
+            ),
             textAlign: TextAlign.center,
           ),
-          res(_sum),
-        ],
-      ),
-    );
+          backgroundColor: Colors.cyanAccent,
+        ),
+        backgroundColor: Colors.limeAccent,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 250, horizontal: 70),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Score : ' + _sum.toString(),
+                style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              res(_sum),
+            ],
+          ),
+        ));
   }
 }
