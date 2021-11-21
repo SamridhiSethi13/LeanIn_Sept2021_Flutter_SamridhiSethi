63b03cdb5286d2ce0eb5dedf4017b2b1fc5bb49e
Samridhi Sethi <90918405+SamridhiSethi13@users.noreply.github.com>
2021-11-22 01:38:43 +0530

    Add files via upload

---
+++ MILESTONE 3/root.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_app/pages/home_page.dart';
+import 'constants/constant.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({ Key key }) : super(key: key);
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+
+  int activeTab = 0;
+  AppBar appbar;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: getAppBar(),
+      bottomNavigationBar: getFooter(),
+      body: getBody(activeTab),
+    );
+  }
+
+ // ignore: missing_return
+ Widget getAppBar() 
+  {
+    switch (activeTab) {
+      case 0:
+        appbar = null;
+        break;
+      case 1:
+        appbar = null;
+        break;
+      case 2:
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white, 
+          elevation: 0.8,
+          title: const Text(
+            "ACCOUNT", 
+            style: TextStyle(
+              color: Colors.black
+            ),
+            textAlign: TextAlign.center,
+          ),
+        );
+      case 3:
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white, 
+          elevation: 0.8,
+          title: const Text(
+            "CART", 
+            style: TextStyle(
+              color: Colors.black
+            ),
+            textAlign: TextAlign.center,
+          ),
+        );
+      case 4:
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white, 
+          elevation: 0.8,
+          title: const Text(
+            "MORE", 
+            style: TextStyle(
+              color: Colors.black              
+            ),
+            textAlign: TextAlign.center,
+          ),
+        );
+      
+    }
+  }
+
+  Widget getFooter()
+  {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(top: BorderSide(color: Colors.grey.withOpacity(0.2)))
+      ),
+      child: Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(itemsTab.length, (index){
+          return IconButton(
+            onPressed: (){
+              setState(() {
+                activeTab = index;
+              });
+            }, 
+            icon: Icon(
+              itemsTab[index]['icon'],
+              size: itemsTab[index]['size'],
+              color: activeTab == index ? Colors.orange : Colors.black,
+            )
+            );
+          
+        }
+      
+      ),
+      )
+      )
+    );
+  }
+}
+
+Widget getBody(int activeTab)
+{
+  return IndexedStack(
+    index: activeTab,
+    children: const [
+      HomePage(),
+      Center(
+        child: Text("Store", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+      ),
+      Center(
+        child: Text("Account", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+      ),
+      Center(
+        child: Text("Cart", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+      ),
+      Center(
+        child: Text("More", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+      )
+    ]    
+  );
+}
